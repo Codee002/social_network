55992de11e1d93f1cbdee799b23564a846bccc37
Code002 <kakashivncm123@gmail.com>
2025-06-14 23:54:04 +0700

    Update Route Api

--- back_end/routes/api.php
+++ back_end/routes/api.php
@@ -2,17 +2,36 @@
 
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\TestController;
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\User\ConversationController;
+use App\Http\Controllers\Api\User\RelationController;
+use App\Http\Controllers\Api\User\UserController;
+use Illuminate\Support\Facades\Broadcast;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
+Broadcast::routes(['middleware' => ['auth:sanctum']]);
 Route::apiResource("/test", TestController::class);
 
 // Auth
 Route::prefix("/auth")->name("auth.")->group(function () {
     Route::post("register", [AuthController::class, 'register'])->name("register");
     Route::post("login", [AuthController::class, 'login'])->name("login");
+    Route::middleware('auth:sanctum')->post("logout", [AuthController::class, 'logout'])->name("logout");
+});
+
+// User
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/owner', [UserController::class, 'getOwner']);
+    Route::get('/user/{id}', [UserController::class, 'getUser']);
+    Route::get('/getRelation/{owner_id}/{user_id}', [UserController::class, 'getRelation']);
+    Route::get('/getFriends/{id}', [UserController::class, 'getFriends']);
+
+    // Relation
+    Route::post('/relation/addRelation/', [RelationController::class, 'addRelation']);
+    Route::post('/relation/changeRelation/', [RelationController::class, 'changeRelation']);
+
+    // Conversation
+    Route::post("conversation/find", [ConversationController::class, "findConversationUser"]);
+    Route::get("conversation/get", [ConversationController::class, "getConversation"]);
+    Route::get("conversation/message/{conversation_id}", [ConversationController::class, "getMessage"]);
+    Route::post("conversation/sendMessage", [ConversationController::class, "sendMessage"]);
 });
--- back_end/routes/channels.php
+++ back_end/routes/channels.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Broadcast;
+use Illuminate\Support\Facades\Log;
 
+Broadcast::routes(['middleware' => ['auth:sanctum']]);
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
+    return (int) $user->id === (int) $id;
+});
+
+Broadcast::channel('user.{id}', function ($user, $id) {
+    Log::info('Broadcast Auth: ' . $user->id . ' vs ' . $id);
+    return (int) $user->id === (int) $id;
+});
+
+Broadcast::channel('test.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
