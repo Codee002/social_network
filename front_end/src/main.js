b63a8b47a29a7371e03014aed1b63295888ef541
Code002 <kakashivncm123@gmail.com>
2025-06-14 23:56:56 +0700

    Update Layout Css, root HTML

--- front_end/src/main.js
+++ front_end/src/main.js
@@ -1,46 +1,53 @@
-import { createApp } from "vue";
-import App from "./App.vue";
+import { createApp } from 'vue'
+import App from './App.vue'
 
 // Pusher Laravel
-import Echo from "laravel-echo";
-import Pusher from "pusher-js";
+import Echo from 'laravel-echo'
+import Pusher from 'pusher-js'
 
-window.Pusher = Pusher;
+window.Pusher = Pusher
 
 window.Echo = new Echo({
-  broadcaster: "pusher",
+  broadcaster: 'pusher',
   key: process.env.VUE_APP_PUSHER_APP_KEY,
-  cluster: "ap1",
-  forceTLS: true,
-});
+  cluster: 'ap1',
+  forceTLS: (process.env.VUE_APP_PUSHER_APP_SCHEME ?? 'https') === 'https',
+  enabledTransports: ['ws', 'wss'],
+  authEndpoint: 'http://127.0.0.1:8000/broadcasting/auth',
+  auth: {
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem('auth_token')}`,
+    },
+  },
+})
 
 // Ant Design Vue
-import Antd  from "ant-design-vue";
-import "ant-design-vue/dist/reset.css";
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/reset.css'
 
 // Vue Toastification
-import Toast from "vue-toastification";
-import "vue-toastification/dist/index.css";
+import Toast from 'vue-toastification'
+import 'vue-toastification/dist/index.css'
 const options = {
-  transition: "Vue-Toastification__bounce",
+  transition: 'Vue-Toastification__bounce',
   maxToasts: 20,
-  newestOnTop: true
-};
-
+  newestOnTop: true,
+}
 
 // Axios
-import axios from "axios";
+import axios from 'axios'
 window.axios = axios
-axios.defaults.withCredentials = true;
+axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('auth_token')}`
+axios.defaults.baseURL = process.env.VUE_APP_DOMAIN_API;
 
 // Router
-import router from "./router/index";
+import router from './router/index'
 
 // CSS
-import "./assets/styles/global.css";
+import './assets/styles/global.css'
 
-const app = createApp(App);
-app.use(Toast, options);
-app.use(router);
-app.use(Antd);
-app.mount("#app");
+const app = createApp(App)
+app.use(Toast, options)
+app.use(router)
+app.use(Antd)
+app.mount('#app')
