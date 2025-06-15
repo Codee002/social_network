f1d2162a0fce49c925c7cc478b92c06f74e82c90
Code002 <kakashivncm123@gmail.com>
2025-06-15 21:14:47 +0700

    Update Route, Channel

--- back_end/routes/channels.php
+++ back_end/routes/channels.php
@@ -9,10 +9,5 @@ Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
 });
 
 Broadcast::channel('user.{id}', function ($user, $id) {
-    Log::info('Broadcast Auth: ' . $user->id . ' vs ' . $id);
-    return (int) $user->id === (int) $id;
-});
-
-Broadcast::channel('test.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
