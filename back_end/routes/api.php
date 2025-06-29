1120eee9344395a8503d05f13a1c87d8460dd0a3
Code002 <kakashivncm123@gmail.com>
2025-06-30 00:31:10 +0700

    Update Route Api

--- back_end/routes/api.php
+++ back_end/routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\TestController;
 use App\Http\Controllers\Api\User\ConversationController;
+use App\Http\Controllers\Api\User\PostController;
 use App\Http\Controllers\Api\User\RelationController;
 use App\Http\Controllers\Api\User\UserController;
 use Illuminate\Support\Facades\Broadcast;
@@ -23,7 +24,10 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/owner', [UserController::class, 'getOwner']);
     Route::get('/user/{id}', [UserController::class, 'getUser']);
     Route::get('/getRelation/{owner_id}/{user_id}', [UserController::class, 'getRelation']);
+    // Route::get('/getFriends/{id}', [UserController::class, 'getFriends']);
+    Route::get('/getPosts/{id}', [UserController::class, 'getPosts']);
     Route::get('/getFriends/{id}', [UserController::class, 'getFriends']);
+    Route::get('/getInvited/{id}', [UserController::class, 'getInvited']);
 
     // Relation
     Route::post('/relation/addRelation/', [RelationController::class, 'addRelation']);
@@ -34,4 +38,13 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get("conversation/get", [ConversationController::class, "getConversation"]);
     Route::get("conversation/message/{conversation_id}", [ConversationController::class, "getMessage"]);
     Route::post("conversation/sendMessage", [ConversationController::class, "sendMessage"]);
+
+    // Post
+    Route::post("post/store", [PostController::class, "store"]);
+    Route::get("post/detail/{post_id}", [PostController::class, "getPostDetail"]);
+    Route::post("post/storeComment/", [PostController::class, "storeComment"]);
+    Route::post("post/storeLike/", [PostController::class, "storeLike"]);
+    Route::post("post/storeView/", [PostController::class, "storeView"]);
+    Route::post("post/storeShare/", [PostController::class, "storeShare"]);
+
 });
