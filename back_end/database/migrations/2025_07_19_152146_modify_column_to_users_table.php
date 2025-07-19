d0837828c5dd810cdea9cee5db4dba5652198da3
Code002 <kakashivncm123@gmail.com>
2025-07-19 23:30:39 +0700

    Update Migration: Add column address, modify value email_active to Users

---
+++ back_end/database/migrations/2025_07_19_152146_modify_column_to_users_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->enum('email_active', [0, 1])->nullable()->default(0)->after('email')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->time("email")->change();
+        });
+    }
+};
